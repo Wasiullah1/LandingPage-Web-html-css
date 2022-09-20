5482c47d18caf66c273711e2af9a7d931c7f35fa
Wasiullah <waseeullah14@gmail.com>
2022-09-20 05:06:29 -0700

    Food Website Landing Page

---
+++ Javascript/restraunt.js
@@ -0,0 +1,22 @@
+ let nav = document.querySelector('.navigation-wrap');
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }
+    else{
+        nav.classList.remove("scroll-on");
+    }
+}
+// document.getElementById('.navigation-wrap', addEventListener("scroll", function(){
+//     if(document.documentElement.scrollTop > 20){
+//         nav.classList.add("scroll-on");
+//     }
+//     else{
+//         nav.classList.remove("scroll-on");
+//     }
+// }))
+      
+
+
+
+
